9055410d46d4ebf7d6bbf8434b744aa18bbea277
CodePy <tiwariparas1096@gmail.com>
2024-10-03 13:26:37 +0530

    trying to fix login issue

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,15 +2,7 @@ import { initializeApp, getApps, FirebaseApp } from 'firebase/app';
 import { getAuth, Auth } from 'firebase/auth';
 import { getFirestore, Firestore } from 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
-};
+import { firebaseConfig } from './config';
 
 let app: FirebaseApp | undefined;
 let auth: Auth | undefined;
